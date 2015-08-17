59a4590e5a410f5bee68c80a3a4239a69438f3ee
JX <JX@Ultra>
2015-08-17 09:24:26 -0700

    1st commit
    Signed-off-by: JX <JX@Ultra>

---
+++ BattStatusParser/BatteryStatusParser.py
@@ -0,0 +1,414 @@
+#Author:  Jeremy Xue
+#Date:    8/6/2015
+import os
+import sys
+import re
+import csv
+from collections import OrderedDict
+
+def FUNC_NAME(some_func):
+    def Inner(*args, **kwargs):
+        print "\n\n...Function Invoked: %s ..." % some_func.func_name            
+        return some_func(*args)
+    return Inner
+
+class BSP():
+    def __init__(self, log):
+        # i.e. log = c:\temp\abc.txt
+        if os.path.exists(log) == False:                
+            self.__exit__('path does not exist: %s' % log)
+        self.logFullPath = log
+        self.logDirectory = os.path.dirname(log) # c:\temp
+        self.logName = os.path.basename(log) # abc.txt
+        self.logfName, self.logExt = os.path.splitext(self.logName) # abc and .txt
+        self.outputName = self.logfName + '_output.csv' # abc_output.csv"
+        self.outputFullPath = self.logDirectory+ '\\' + self.outputName # c:\temp\abc_output.txt
+        self.fieldNames = ["Th", "UUT", "IPv6", "MAC ID", 
+                           "Time Stamp",
+                           "Last Battery Test",
+                           "Stamp", 
+                           "Pre-Test Charger Temp",
+                           "Pre-Test Batt Temp",                                                                                               
+                           "Pre-Test Batt Volt",                                                                                                
+                           "Post-Test Charger Temp",                                                                               
+                           "Post-Test Batt Temp",                                                                                               
+                           "Post-Test Batt Volt",                                                                                                
+                           "Power Mode",                                              
+                           "Present Charger Temp",                                                                                               
+                           "Present Batt Temp",                                                                                               
+                           "Present Batt Volt",                                                                                               
+                           "Present Batt Current",                                                                                              
+                           "Time since last batt ev"
+                           ]
+        self.uniqueLastBattTest = ''
+        
+
+        
+        #search started flag
+        self.ssed = False
+    
+    def __exit__(self, msg=''):
+        sys.exit("\n\n %s \n... Good bye ...\n" % msg)
+        
+    @FUNC_NAME
+    def FileNames(self):        
+        print "{0:.<25}: {1:<100}".format("log file directory", self.logDirectory)
+        print "{0:.<25}: {1:<100}".format("log file name", self.logName)
+        #print "{0:.<25}: {1:<100}".format("log file name", self.logfName)
+        #print "{0:.<25}: {1:<100}".format("log extension", self.logExt)
+        print "{0:.<25}: {1:<100}".format("output file name", self.outputName)
+        print "{0:.<25}: {1:<100}".format("output file full path", self.outputFullPath)
+    
+    
+    
+    @FUNC_NAME
+    def OutputFunc(self, log):
+        '''
+            nLine 1: REL_B:  fe80::213:50ff:fe11:e000
+            nLine 2: 288
+            nLine 3: Thu Aug  6 14:45:56 PDT 2015
+            nLine 4: *******      Battery Status      *******
+            nLine 5: Last Battery Test       :  Wed Aug  5 14:55:42 2015
+            nLine 6: ******* Status before load test  *******
+            nLine 7: Charger Temperature     :  31.00 C.
+            nLine 8: Battery Temperature     :  23.36 C.
+            nLine 9: Battery Voltage         :  9.55 V.
+            nLine 10: *******  Status after load test  *******
+            nLine 11: Charger Temperature     :  31.00 C.
+            nLine 12: Battery Temperature     :  22.82 C.
+            nLine 13: Battery Voltage         :  9.55 V.
+            nLine 14: *******      Present State       *******
+            nLine 15: Power Mode              :  Battery is OFF; device is operating on external power.
+            nLine 16: Charger Temperature     :  31.37 C.
+            nLine 17: Battery Temperature     :  25.87 C.
+            nLine 18: Battery Voltage         :  12.39 V.
+            nLine 19: Battery Charge Current  :  400.18 mA.
+            nLine 20: Time since last batt ev :  172321 seconds.
+        '''
+        with open(log, 'r') as dataFile:
+            #for testing
+            with open(self.outputFullPath, 'w') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=self.fieldNames, lineterminator='\n', )
+                writer.writeheader()
+                
+                line = dataFile.next()
+                t = 0
+                while True:                                  
+                    if self.GetMACID(line) is not None:
+                        t = t + 1
+                        HR = t / 6
+                        UUT, IPV6, MAC = self.GetMACID(line)
+                        row = OrderedDict({"Th": HR, "UUT": UUT, "IPv6":IPV6, "MAC ID": MAC})                        
+                        updateLastBatTest = False
+                        nLine = 1
+                        for field in self.fieldNames[4:]:
+                            line = dataFile.next()
+                            nLine = nLine + 1
+                                               
+                            if nLine in [2, 4, 10, 14]:
+                                nLine = nLine + 1
+                                line = dataFile.next()
+                            
+                            if nLine in [7,8,9,11,12,13,16,17,18,19]:
+                                data = self.GetData(line)
+                                row.update({field:data})
+                                continue
+                            
+                            if nLine == 20:
+                                lastBattEv = self.GetBattEvent(line)
+                                row.update({field:lastBattEv})
+                                continue
+
+                            if nLine == 15:
+                                pwrMode = self.GetPwrMode(line)
+                                row.update({field:pwrMode})
+                                continue
+                            
+                            if nLine == 6:
+                                if updateLastBatTest == True:
+                                    row.update({field:"x"})
+                                else:
+                                    row.update({field:""})                                                                    
+                                continue
+                            
+                            if nLine == 5:
+                                LastBatteryTest, LBTUnique = self.GetLastBatTest(line)
+                                if LBTUnique != self.uniqueLastBattTest:
+                                    self.uniqueLastBattTest = LBTUnique
+                                    updateLastBatTest = True
+                                else:
+                                    updateLastBatTest = False
+                                row.update({field:LastBatteryTest})
+                                continue
+                            
+                            if nLine == 3:
+                                TimeStamp = self.GetTimeStamp(line)
+                                row.update({field:TimeStamp})
+                                continue                                                                                                
+                            
+                        print row
+                        writer.writerow(row)                        
+                                                                
+                    else:
+                        try:
+                            line = dataFile.next()
+                            print 
+                        except StopIteration:
+                            print "...End of file..."
+                            return False
+    
+    #Not in use
+    @FUNC_NAME    
+    def YieldLine(self, log):        
+        with open(log, 'r') as dataFile:
+            #for testing
+            with open(self.outputFullPath, 'w') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames = self.fieldNames)
+                writer.writeheader()
+                
+                line = dataFile.next()
+                while True:                
+                    if self.GetMACID(line) is not None:
+                        
+                        UUT, IPV6, MAC = self.GetMACID(line)                        
+                        print "{0:.<25}: {1:<100}\n{2:.<25}: {3:<100}\n{4:.<25}: {5:<100}".format("UUT: ", UUT, "IPV6: ", IPV6, "MAC: ", MAC)
+                        #writer.writerow({"UUT": UUT, "IPv6": IPV6, "MAC": MAC})
+                        
+                        dataFile.next() #count number
+                        
+                        line = dataFile.next() #time stamp
+                        TimeStamp = self.GetTimeStamp(line)
+                        print "{0:.<25}: {1:<100}".format("Time stamp: ", TimeStamp)
+                        
+                        
+                        dataFile.next() # ** Battery Status **
+                        
+                        line = dataFile.next() # last battery test
+                        print "{0:.<25}: {1:<100}".format("Last Battery Test: ", self.GetLastBatTest(line))
+                         
+                        dataFile.next() # ** Status before load test **
+                        
+                        line = dataFile.next() # Charger Temp
+                        print "{0:.<25}: {1:<100}".format("Pre-Test Charger Temp: ", self.GetData(line))
+                        
+                        line = dataFile.next() # Battery Temp
+                        print "{0:.<25}: {1:<100}".format("Pre-Test Batt Temp: ", self.GetData(line))
+                        
+                        line = dataFile.next() # Battery Voltage
+                        print "{0:.<25}: {1:<100}".format("Pre-Test Batt Volt: ", self.GetData(line))
+                        
+                        dataFile.next() # ** Status after load test **
+                        
+                        line = dataFile.next() # Charger Temp
+                        print "{0:.<25}: {1:<100}".format("Post-Test Charger Temp: ", self.GetData(line))
+                        
+                        line = dataFile.next() # Battery Temp
+                        print "{0:.<25}: {1:<100}".format("Post-Test Batt Temp: ", self.GetData(line))
+                        
+                        line = dataFile.next() # Battery Voltage
+                        print "{0:.<25}: {1:<100}".format("Post-Test Batt Volt: ", self.GetData(line))
+                        
+                        dataFile.next() # ** Present Status **
+                        
+                        line = dataFile.next() # Power Mode
+                        print "{0:.<25}: {1:<100}".format("Power Mode: ", self.GetPwrMode(line))
+                        
+                        line = dataFile.next() # Charger Temp
+                        print "{0:.<25}: {1:<100}".format("Present Charger Temp: ", self.GetData(line))
+                        
+                        line = dataFile.next() # Battery Temp                    
+                        #print "Post-Test Batt Temp: ", self.GetData(line)
+                        print "{0:.<25}: {1:<100}".format("Present Batt Temp: ", self.GetData(line))
+                        
+                        line = dataFile.next() # Battery Voltage                    
+                        print "{0:.<25}: {1:<100}".format("Present Batt Volt: ", self.GetData(line))
+                        
+                        line = dataFile.next() # Battery Current                   
+                        print "{0:.<25}: {1:<100}".format("Present Batt Current: ", self.GetData(line))
+    
+                        line = dataFile.next() # Time since last batt ev
+                        print "{0:.<25}: {1:<100}".format("Time since last batt ev: ", self.GetBattEvent(line))                                    
+                                                                
+                    else:
+                        try:
+                            line = dataFile.next()
+                            print 
+                        except StopIteration:
+                            print "...End of file..."
+                            return False
+            
+                
+    #@FUNC_NAME
+    def ConvIpMac(self, input):
+        #determine its IPv6 or MacID
+        ipv6Input = re.compile("([0-9a-fA-F]{4}::[0-9a-fA-F]{3}:([0-9a-fA-F]{4}:){2}[0-9a-fA-F]{4})")
+        macInput = re.compile("([0-9a-fA-F]{2}:{0,1}){7}[0-9a-fA-F]{2}")
+        macInput2 = re.compile("[0-9a-fA-F]{16}")
+                
+        if ipv6Input.match(input) is not None:
+            #print input
+            mac = re.sub(':', '', input, flags=re.IGNORECASE)
+            mac = re.sub('^fe802', '00', mac, flags=re.IGNORECASE)
+            return mac.upper() # return mac ID in upper case
+        elif (macInput.match(input) or macInput2.match(input)) is not None:
+            #print input
+            ipv6 = re.sub(':', '', input, flags=re.IGNORECASE)
+            ipv6 = re.sub('^00', 'fe802', ipv6, flags=re.IGNORECASE)
+            ipv6 = re.sub('(?<=^([0-9a-f]){4})','::', ipv6, flags=re.IGNORECASE)
+            ipv6 = re.sub('(?<=^([0-9a-f:]){9})',':', ipv6, flags=re.IGNORECASE)
+            ipv6 = re.sub('(?<=^([0-9a-f:]){14})',':', ipv6, flags=re.IGNORECASE)
+            ipv6 = re.sub('(?<=^([0-9a-f:]){19})',':', ipv6, flags=re.IGNORECASE)
+            return ipv6
+        else:
+            print "No match for neither IPv6 nor MAC ID"
+            return None
+                
+    
+    #@FUNC_NAME
+    def GetMACID(self, line, printInfo=0):
+        '''
+            i.e.
+            line = "REL_B:  fe80::213:50ff:fe11:e000"
+            return "REL_B", "fe80::213:50ff:fe11:e000", "1350FFFE11E000"
+        '''
+        #regex for
+        #REL_B:  fe80::213:50ff:fe11:e000
+        #match REL_B: (^(\w*|\d*|_*)(?=:\s*fe80))
+        #match IPV6: ([0-9a-fA-F]{4}::[0-9a-fA-F]{3}:([0-9a-fA-F]{4}:){2}[0-9a-fA-F]{4})
+#    if self.ssed == False:
+        uutInput = re.compile("(^(\w*|\d*|_*)(?=:\s*fe80))")
+        ipv6Input = re.compile("([0-9a-fA-F]{4}::[0-9a-fA-F]{3}:([0-9a-fA-F]{4}:){2}[0-9a-fA-F]{4})")            
+        uut = uutInput.match(line)
+        ipv6 = ipv6Input.search(line)            
+        if uut is not None:
+#            self.ssed = True
+            #print line,
+            #print uut.group(0)
+            #print ipv6.group(0)
+            #print self.ConvIpMac(ipv6.group(0))
+            return uut.group(0), ipv6.group(0), self.ConvIpMac(ipv6.group(0))
+
+    
+    def GetTimeStamp(self, line):
+        '''
+            i.e.
+            line = Tue Aug  4 16:33:02 PDT 2015
+            return "Tue", "Aug", 4, 16, 33, 02, "PDT", 2015
+        '''
+        #regex for
+        #Mon Tue Wed Thr Fri Sat Sun ^((Mon)|(Tue)|(Wed)|(Thr)|(Fri)|(Sat)|(Sun))
+        #Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec 
+        #
+        #
+#         timeStamp = re.compile(('(?P<wday>^((Mon)|(Tue)|(Wed)|(Thr)|(Fri)|(Sat)|(Sun))'
+#                                 '(?P<month>(?=\s)((Jan)|(Feb)|(Mar)|(Apr)|(May)|(Jun)|(Jul)|(Aug)|(Sep)|(Oct)|(Nov)|(Dec))'
+#                                 '(?P<day>'
+#                                 '(?P<sec>\d{2}(\.\d{3}|)))'
+#                                 , line))
+        Result = line.strip('\n')
+        return Result
+
+    
+    def GetPwrMode(self, line):
+        '''
+            i.e.
+            line = "Power Mode              :  Battery is OFF; device is operating on external power."
+            return "Battery is OFF; device is operating on external power."
+        '''
+        Input = re.compile("(?<=^Power Mode              :  ).*")        
+        Result = Input.search(line)        
+        if Result is not None:            
+            return Result.group(0)
+    
+    def GetData(self, line):
+        '''
+            i.e.
+            line = "Charger Temperature     :  31.12 C."
+            return 31.12
+        '''
+        Input = re.compile("(?<=^[\w\s:]{27})\d{1,3}.\d{1,2}")        
+        Result = Input.search(line)        
+        if Result is not None:
+            return Result.group(0)
+    
+    def GetChrTemp(self, line):
+        '''
+            i.e.
+            line = "Charger Temperature     :  31.12 C."
+            return 31.12
+        '''
+        Input = re.compile("(?<=^Charger Temperature     :  )\d{1,2}.\d{1,2}")        
+        Result = Input.search(line)        
+        if Result is not None:
+            return Result.group(0)
+    
+    
+    def GetBatTemp(self, line):
+        '''
+            i.e.
+            line = "Battery Temperature     :  31.12 C."
+            return 31.12
+        '''
+        Input = re.compile("(?<=^Battery Temperature     :  )\d{1,2}.\d{1,2}")        
+        Result = Input.search(line)        
+        if Result is not None:
+            return Result.group(0)
+    
+    
+    def GetBatVolt(self, line):
+        '''
+            i.e.
+            line = "Battery Voltage         :  1.12 V."
+            return 1.12
+        '''
+        Input = re.compile("(?<=^Battery Voltage         :  )\d{1,2}.\d{1,2}")        
+        Result = Input.search(line)        
+        if Result is not None:
+            return Result.group(0)
+    
+    
+    def GetBatCurr(self, line):
+        '''
+            i.e.
+            line = "Battery Charge Current  :  11.12 mA."
+            return 11.12
+        '''
+        Input = re.compile("(?<=^Battery Charge Current  :  )\d{1,2}.\d{1,2}")        
+        Result = Input.search(line)        
+        if Result is not None:
+            return Result.group(0)
+
+    def GetLastBatTest(self, line):
+        '''
+            i.e.
+            line = "Last Battery Test       :  Tue Aug  4 14:54:42 2015"
+            return  "Tue Aug  4 14:54:42 2015", "Tue Aug  4 14:54 2015"
+        '''        
+        Input = re.compile("(?<=^Last Battery Test       :  ).*")
+        Result = Input.search(line)
+        LastBatDate = re.sub(':\d{2}\s', '', Result.group(0), flags=re.IGNORECASE)
+        if Result is not None:
+            return Result.group(0), LastBatDate
+    
+    
+    def GetBattEvent(self, line):
+        '''
+            i.e.
+            line = "Time since last batt ev :  3004 seconds."
+            return 3004
+        '''
+        #regex for
+        #(?<=^Time since last batt ev :  )\d{1,}
+        Input = re.compile("(?<=^Time since last batt ev :  )\d{1,}")        
+        Result = Input.search(line)        
+        if Result is not None:            
+            return Result.group(0)
+    
+    @FUNC_NAME
+    def Test(self):
+        for i in self.YieldLine(self.logFullPath):
+            #print i
+            print self.GetMACID(i[1])
+            print self.GetBattEvent(i[1])
+
+    
---
+++ BattStatusParser/main.py
@@ -0,0 +1,91 @@
+from BatteryStatusParser import BSP
+
+testLog = r"C:\Users\jxue\Documents\RMA\RMA 35491\Net_Mgr_Logs\Scenario2_BBU_0p25V.txt"
+
+test = BSP(testLog)
+
+test.FileNames()
+
+#test.Test()
+
+ipv6 = "fe80::213:50ff:fe11:e000"
+macID = "00:13:50:ff:fe:11:e0:00"
+macID2 = "001350fffe11e000"
+NoneSense1 = "001350fffe11e00k0asd"
+NoneSense2 = "001350"
+NoneSense3 = "001350fff"
+
+#for i in [ipv6, macID, macID2, NoneSense1, NoneSense2, NoneSense3]:
+#    print test.ConvIpMac(i)
+
+
+test.OutputFunc(testLog)
+# print next(test.YieldLine(testLog))
+# print next(test.YieldLine(testLog))
+# print next(test.YieldLine(testLog))
+# print next(test.YieldLine(testLog))
+
+
+
+
+#########
+# 
+# def logger(func):
+#     def inner(*args, **kwargs):
+#         print "Arguments: %s, %s" % (args, kwargs)
+#         return func(*args, **kwargs)
+#     return inner
+# 
+# @logger
+# def fool(x, y=1):
+#     return x * y
+
+# 
+# 
+# 
+# class Coordinate(object):
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#     
+#     def __repr__(self):
+#         return "Coord: " + str(self.__dict__)
+#     
+# def add(a, b):
+#     return Coordinate(a.x + b.x,  a.y + b.y)
+# 
+# def sub(a, b):
+#     return Coordinate(a.x - b.x,  a.y - b.y)
+# 
+# one = Coordinate(100, 200)
+# two = Coordinate(300, 200)
+# 
+# add(one, two)
+# 
+# 
+# def wrapper(func):
+#     def checker(a, b):
+#         if a.x < 0 or a.y < 0:
+#             a = Coordinate(a.x if a.x > 0 else 0, a.y if a.y > 0 else 0)
+#         if b.x < 0 or b.y < 0:
+#             b = Coordinate(b.x if b.x > 0 else 0, b.y if b.y > 0 else 0)
+#         ret = func(a, b)
+#         if ret.x < 0 or ret.y < 0:
+#             ret = Coordinate(ret.x if ret.x > 0 else 0, ret.y if ret.y > 0 else 0)
+#         return ret
+#     return checker
+# 
+# 
+# #########
+# def outer(some_func):
+#     def inner():
+#         print "before some_func"
+#         print some_func.func_name
+#         return some_func()    
+#     return inner
+# 
+# def foo():
+#     print "this is foo"
+#     
+# decorated = outer(foo)
+# decorated()
